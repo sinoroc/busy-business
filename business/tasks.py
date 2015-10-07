1483b46869c4ec35dde7845b5505a394d23ed6bc
sinoroc <sinoroc@users.noreply.github.com>
2015-10-07 11:00:45 +0200

    Add meta info

--- business/tasks.py
+++ business/tasks.py
@@ -2,29 +2,41 @@
 """
 
 
+import billiard
 import celery
+import socket
 import time
 
 
-app = celery.Celery('business.tasks')
+celery_app = celery.Celery('business.tasks', backend='redis://redis')
+
+host_name = socket.gethostname()
 
 
-@app.task
+@celery.task
 def foo(counter):
     print("Executing task foo...")
+    process = billiard.current_process()
+    worker_name = process.initargs[1]
+    process_index = process.index
     for i in range(0, counter):
         print("Foo [{}]".format(i))
         time.sleep(1)
-    return i + 1
+    return {
+        'host': host_name,
+        'worker': worker_name,
+        'process': process_index,
+        'value': i + 1,
+    }
 
 
-@app.task
+@celery.task
 def foo_callback(result):
     print("Task foo successfully executed. Result: [{}].".format(result))
     return None
 
 
-@app.task
+@celery.task
 def bar():
     print("Executing task bar...")
     return None
